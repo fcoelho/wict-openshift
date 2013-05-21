120518d81bb5013dda0dce64bea80cf0e0458530
Felipe Bessa Coelho <fcoelho.9@gmail.com>
2013-05-21 15:10:03 -0300

    wict submodule and setup.py script ready

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
+# coding: utf-8
+
 from setuptools import setup
 
-setup(name='YourAppName', version='1.0',
-      description='OpenShift Python-2.7 Community Cartridge based application',
-      author='Your Name', author_email='ramr@example.org',
-      url='http://www.python.org/sigs/distutils-sig/',
-
-      #  Uncomment one or more lines below in the install_requires section
-      #  for the specific client drivers/modules your application needs.
-      install_requires=['greenlet', 'gevent',
-                        #  'MySQL-python',
-                        #  'pymongo',
-                        #  'psycopg2',
-      ],
-     )
+setup(
+	name='WICT',
+	version='0.1',
+	description='Sistema de revisão para o VII WICT',
+	author='Felipe Bessa Coelho',
+	author_email='fcoelho.9@gmail.com',
+	url='http://bitbucket.org/fcoelho/wict',
+	install_requires=[
+		'greenlet',
+		'gevent',
+		'Django==1.5.1',
+		'Unipath==1.0',
+		'django-debug-toolbar==0.9.4',
+		'django-sendfile==0.3.0',
+		'pyPdf==1.13',
+		'django-signup==0.1',
+	],
+	dependency_links = [
+		'https://bitbucket.org/fcoelho/django-signup/get/tip.zip#egg=django-signup-0.1'
+	]
+)
